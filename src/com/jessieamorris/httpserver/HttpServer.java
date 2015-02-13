a559be73f304a815d418298c58189bed2e2f21c0
Jessie A. Morris <jessie.morris@finicity.com>
2015-02-12 18:28:16 -0700

    Migrate code from Main to HttpServer.

---
+++ src/com/jessieamorris/httpserver/HttpServer.java
@@ -0,0 +1,83 @@
+package com.jessieamorris.httpserver;
+
+import com.jessieamorris.httpserver.exceptions.HttpException;
+import com.jessieamorris.httpserver.exceptions.NotFoundException;
+import com.jessieamorris.httpserver.exceptions.NotImplementedException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by jessie.
+ */
+public class HttpServer {
+	public HttpServer(int portNumber) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(portNumber);
+
+			System.out.println("Server started");
+
+			Socket clientSocket = null;
+
+			while ((clientSocket = serverSocket.accept()) != null) {
+				System.out.println("Connection made?");
+
+				OutputStream out = clientSocket.getOutputStream();
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+				HttpRequest request = new HttpRequest();
+				HttpResponse response = new HttpResponse(out);
+
+				try {
+					request.parseRequest(in);
+
+					if (request.getMethod() == HttpRequest.Method.GET) {
+						URI requestUri = request.getURI();
+
+						// TODO: Security issues, could escalate by using "../" in paths
+						Path path = Paths.get("./", requestUri.getPath());
+
+						System.out.println("Trying to get a file at: " + path);
+
+						Charset charset = Charset.forName("US-ASCII");
+						response.setBodyReader(Files.newBufferedReader(path, charset));
+
+						String mimeType = Files.probeContentType(path);
+						if (mimeType == null) {
+							mimeType = "text/plain";
+						}
+
+						response.setHeader(new Header("Content-Type", mimeType));
+
+						response.handleRequest(request);
+					} else {
+						response.handleException(new NotImplementedException());
+					}
+				} catch (HttpException e) {
+					response.handleException(e);
+				} catch (NoSuchFileException e) {
+					response.handleException(new NotFoundException());
+				} catch (IOException e) {
+					response.handleException(new NotFoundException());
+				} finally {
+					in.close();
+					out.close();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/com/jessieamorris/httpserver/Main.java
+++ src/com/jessieamorris/httpserver/Main.java
@@ -1,19 +1,5 @@
 package com.jessieamorris.httpserver;
 
-import com.jessieamorris.httpserver.exceptions.HttpException;
-import com.jessieamorris.httpserver.exceptions.NotFoundException;
-import com.jessieamorris.httpserver.exceptions.NotImplementedException;
-
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.URI;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /**
  * Created by jessie on 15-02-05.
  */
@@ -26,62 +12,7 @@ public class Main {
 			portNumber = Integer.parseInt(args[0]);
 		}
 
-		try {
-			ServerSocket serverSocket = new ServerSocket(portNumber);
-
-			System.out.println("Server started");
-
-			Socket clientSocket = null;
-
-			while((clientSocket = serverSocket.accept()) != null) {
-
-				System.out.println("Connection made?");
-
-				OutputStream out = clientSocket.getOutputStream();
-				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-				HttpRequest request = new HttpRequest();
-				HttpResponse response = new HttpResponse(out);
-
-				try {
-					request.parseRequest(in);
-
-					if(request.getMethod() == HttpRequest.Method.GET) {
-						URI requestUri = request.getURI();
-
-						// TODO: Security issues, could escalate by using "../" in paths
-						Path path = Paths.get("./", requestUri.getPath());
-
-						System.out.println("Trying to get a file at: " + path);
-
-						Charset charset = Charset.forName("US-ASCII");
-						response.setBodyReader(Files.newBufferedReader(path, charset));
-
-						String mimeType = Files.probeContentType(path);
-						if(mimeType == null) {
-							mimeType = "text/plain";
-						}
-
-						response.setHeader(new Header("Content-Type", mimeType));
-
-						response.handleRequest(request);
-					} else {
-						response.handleException(new NotImplementedException());
-					}
-				} catch (HttpException e) {
-					response.handleException(e);
-				} catch (NoSuchFileException e) {
-					response.handleException(new NotFoundException());
-				} catch (IOException e) {
-					response.handleException(new NotFoundException());
-				} finally {
-					in.close();
-					out.close();
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		new HttpServer(portNumber);
 	}
 
 
